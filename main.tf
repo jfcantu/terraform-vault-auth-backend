e111dc02409a8449756328d01990759cc1c9f3e1
John Cantu <john.cantu@dreambox.com>
2020-03-28 18:16:55 -0700

    no message

--- main.tf
+++ main.tf
@@ -3,14 +3,17 @@ resource "vault_auth_backend" "auth" {
     path = var.path
     description = var.description
     local = var.local
-    tune {
-        default_lease_ttl = lookup(var.tune, "default_least_ttl", null)
-        max_lease_ttl = lookup(var.tune, "max_lease_ttl", null)
-        audit_non_hmac_response_keys = lookup(var.tune, "audit_non_hmac_response_keys", [])
-        audit_non_hmac_request_keys = lookup(var.tune, "audit_non_hmac_request_keys", [])
-        listing_visibility = lookup(var.tune, "listing_visibility", "")
-        passthrough_request_headers = lookup(var.tune, "passthrough_request_headers", [])
-        allowed_response_headers = lookup(var.tune, "allowed_response_headers", [])
-        token_type = lookup(var.tune, "token_type", "")
+    dynamic "tune" {
+        for_each = (var.tune == null ? [] : [var.tune])
+        content {
+            default_lease_ttl = tune.value["default_lease_ttl"]
+            max_lease_ttl = tune.value["max_lease_ttl"]
+            audit_non_hmac_response_keys = tune.value["audit_non_hmac_response_keys"]
+            audit_non_hmac_request_keys = tune.value["audit_non_hmac_request_keys"]
+            listing_visibility = tune.value["listing_visibility"]
+            passthrough_request_headers = tune.value["passthrough_request_headers"]
+            allowed_response_headers = tune.value["allowed_response_headers"]
+            token_type = tune.value["token_type"]
+        }
     }
 }
