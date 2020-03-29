652d20c0367b06be2520f17544c68530c11e0f86
John Cantu <john.cantu@dreambox.com>
2020-03-28 18:12:34 -0700

    no message

--- main.tf
+++ main.tf
@@ -3,5 +3,14 @@ resource "vault_auth_backend" "auth" {
     path = var.path
     description = var.description
     local = var.local
-    tune = var.tune
+    tune {
+        default_lease_ttl = lookup(var.tune, "default_least_ttl", null)
+        max_lease_ttl = lookup(var.tune, "max_lease_ttl", null)
+        audit_non_hmac_response_keys = lookup(var.tune, "audit_non_hmac_response_keys", [])
+        audit_non_hmac_request_keys = lookup(var.tune, "audit_non_hmac_request_keys", [])
+        listing_visibility = lookup(var.tune, "listing_visibility", "")
+        passthrough_request_headers = lookup(var.tune, "passthrough_request_headers", [])
+        allowed_response_headers = lookup(var.tune, "allowed_response_headers", [])
+        token_type = lookup(var.tune, "token_type", "")
+    }
 }
